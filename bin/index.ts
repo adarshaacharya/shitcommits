35536d969eb78b304dcb1a1e14aaaeccc0e8a866
maxiums <connectwithadarsha@gmail.com>
2021-05-11 18:20:42 +0545

    chore: remove xo linter

--- bin/index.ts
+++ bin/index.ts
@@ -1,74 +1,74 @@
-import simpleGit, { SimpleGit } from 'simple-git';
 import dns from 'dns';
-import ora from 'ora';
+import {argv} from 'process';
 import updateNotifier from 'update-notifier';
-import pkg from '../package.json';
-import { argv } from 'process';
+import ora from 'ora';
+import simpleGit, {SimpleGit} from 'simple-git';
 import got from 'got';
 import logUpdate from 'log-update';
 import chalk from 'chalk';
+import pkg from '../package.json';
 
-updateNotifier({ pkg }).notify();
+updateNotifier({pkg}).notify();
 
 const git: SimpleGit = simpleGit();
 const spinner = ora();
-const url = `http://whatthecommit.com/index.txt`;
+const url = 'http://whatthecommit.com/index.txt';
 const arg = argv[2];
 
-// chalk theme
+// Chalk theme
 const Error = chalk.bold.red;
 
 if (arg) {
-  if (arg === '-h' || arg === '--help') {
-    console.log(`
+	if (arg === '-h' || arg === '--help') {
+		console.log(`
       usage: wtc
     `);
-  } else {
-    logUpdate(Error(`Wtf are you even typing ${arg}`));
-    process.exit(1);
-  }
+	} else {
+		logUpdate(Error(`Wtf are you even typing ${arg}`));
+		process.exit(1);
+	}
 }
 
-dns.lookup('whatthecommit.com', (err) => {
-  if (err) {
-    logUpdate(Error(`\Connect device to internet mofos!\n`));
-    process.exit(1);
-  } else {
-    logUpdate();
-    spinner.text = 'Fetching commit..\n';
-    spinner.start();
-  }
+dns.lookup('whatthecommit.com', error => {
+	if (error) {
+		logUpdate(Error('\Connect device to internet mofos!\n'));
+		process.exit(1);
+	} else {
+		logUpdate();
+		spinner.text = 'Fetching commit..\n';
+		spinner.start();
+	}
 });
 
 function handleAdd() {
-  spinner.text = 'staging files.\n';
-  spinner.start();
-  spinner.succeed();
-  spinner.stop();
+	spinner.text = 'staging files.\n';
+	spinner.start();
+	spinner.succeed();
+	spinner.stop();
 }
 
 function handleCommit() {
-  spinner.text = 'committing files.\n';
-  spinner.start();
-  spinner.succeed();
-  spinner.stop();
+	spinner.text = 'committing files.\n';
+	spinner.start();
+	spinner.succeed();
+	spinner.stop();
 }
 
 (async function gitAddCommit() {
-  try {
-    let { body } = await got(url);
-    // replace line break at the end of body
-    body = body.replace(/[\n\r]+ */g, '');
+	try {
+		let {body} = await got(url);
+		// Replace line break at the end of body
+		body = body.replace(/[\n\r]+ */g, '');
 
-    spinner.succeed();
-    spinner.stop();
+		spinner.succeed();
+		spinner.stop();
 
-    await git.add(['.'], handleAdd);
-    await git.commit(body, handleCommit);
+		await git.add(['.'], handleAdd);
+		await git.commit(body, handleCommit);
 
-    logUpdate(chalk.yellowBright(`\ngit commit -m"${body}"\n`));
-  } catch (error) {
-    logUpdate(Error(`Wtf ! ${error}`));
-    process.exit(1);
-  }
+		logUpdate(chalk.yellowBright(`\ngit commit -m"${body}"\n`));
+	} catch (error) {
+		logUpdate(Error(`Wtf ! ${error}`));
+		process.exit(1);
+	}
 })();
