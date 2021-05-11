22266c16a19f1c0c3c5e5fbeff2ed16456f27bc4
maxiums <connectwithadarsha@gmail.com>
2021-05-11 13:15:28 +0545

    yo recipes

--- bin/index.ts
+++ bin/index.ts
@@ -1,2 +1,59 @@
-// Import simpleGit, {SimpleGit} from 'simple-git';
-// const git: SimpleGit = simpleGit();
+import simpleGit, { SimpleGit } from 'simple-git';
+import dns from 'dns';
+import ora from 'ora';
+import updateNotifier from 'update-notifier';
+import pkg from '../package.json';
+import { argv } from 'process';
+import got from 'got';
+
+updateNotifier({ pkg }).notify();
+
+const git: SimpleGit = simpleGit();
+const spinner = ora();
+const url = `http://whatthecommit.com/index.txt`;
+
+if (argv[2] === '-h' || argv[2] === '--help') {
+  console.log(`
+    usage: wtc
+  `);
+} else {
+  console.log(`Unknown option ${argv[2]}`);
+}
+
+dns.lookup('whatthecommit.com', (err) => {
+  if (err) {
+    console.log(`\nCheck internet connection!\n`);
+    process.exit(1);
+  } else {
+    spinner.text = 'What the actual fuck?';
+    spinner.start();
+  }
+});
+
+setTimeout(() => {
+  spinner.color = 'yellow';
+  spinner.text = 'Loading rainbows';
+}, 1000);
+
+function handleAdd() {
+  spinner.text = 'staging the files.';
+}
+
+function handleCommit(body: string) {
+  spinner.text = `commit msg : ${body}`;
+}
+
+async function gitAddCommit() {
+  try {
+    const { body }: { body: string } = await got(url);
+    await git.add(['.'], handleAdd);
+    await git.commit(body, () => handleCommit(body));
+    spinner.stop();
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+
+gitAddCommit()
