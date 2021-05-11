41109a638aafb732a83124f264910ed7216c272a
maxiums <connectwithadarsha@gmail.com>
2021-05-11 16:39:39 +0545

    fix: idk what i fixed

--- bin/index.ts
+++ bin/index.ts
@@ -15,54 +15,60 @@ const spinner = ora();
 const url = `http://whatthecommit.com/index.txt`;
 const arg = argv[2];
 
+// chalk theme
+const Error = chalk.bold.red;
+
 if (arg) {
   if (arg === '-h' || arg === '--help') {
     console.log(`
       usage: wtc
     `);
   } else {
-    logUpdate(chalk.bold.red(`Unknown option ${arg}`));
-    process.exit(0);
+    logUpdate(Error(`Wtf are you even typing ${arg}`));
+    process.exit(1);
   }
 }
 
-// if (process.argv.length <= 2) {
-//   console.log(`Unknown agrument`);
-//   process.exit(0);
-// }
-
 dns.lookup('whatthecommit.com', (err) => {
   if (err) {
-    logUpdate(`\Connect device to internet mofos!\n`);
+    logUpdate(Error(`\Connect device to internet mofos!\n`));
     process.exit(1);
   } else {
     logUpdate();
-    spinner.text = 'Loading..\n';
+    spinner.text = 'Fetching commit..\n';
     spinner.start();
   }
 });
 
 function handleAdd() {
-  spinner.color = 'yellow';
   spinner.text = 'staging files.\n';
+  spinner.start();
+  spinner.succeed();
+  spinner.stop();
 }
 
 function handleCommit() {
   spinner.text = 'committing files.\n';
+  spinner.start();
+  spinner.succeed();
+  spinner.stop();
 }
 
-export async function gitAddCommit() {
+(async function gitAddCommit() {
   try {
-    const { body }: { body: string } = await got(url);
-    await git.add(['.'], handleAdd);
-    // await git.commit(body, handleCommit);
+    let { body } = await got(url);
+    // replace line break at the end of body
+    body = body.replace(/[\n\r]+ */g, '');
+
     spinner.succeed();
-    logUpdate(chalk.blue.bold(`\ncommit msg : ${body}\n`));
     spinner.stop();
+
+    await git.add(['.'], handleAdd);
+    await git.commit(body, handleCommit);
+
+    logUpdate(chalk.yellowBright(`\ngit commit -m"${body}"\n`));
   } catch (error) {
-    logUpdate(error);
+    logUpdate(Error(`Wtf ! ${error}`));
     process.exit(1);
   }
-}
-
-gitAddCommit();
+})();
